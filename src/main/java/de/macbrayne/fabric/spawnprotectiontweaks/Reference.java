2452a08fbb52eebc6a953e3d2f718ed5edd6c331
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2021-08-30 20:23:14 +0300

    remove all client side code remove LanguageMixin (thanks again @shedaniel for the code) remove custom networking packets move language files from assets into data as part of the Server Language API migration

--- src/main/java/de/macbrayne/fabric/spawnprotectiontweaks/Reference.java
+++ src/main/java/de/macbrayne/fabric/spawnprotectiontweaks/Reference.java
@@ -2,14 +2,12 @@ package de.macbrayne.fabric.spawnprotectiontweaks;
 
 import de.macbrayne.fabric.spawnprotectiontweaks.utils.ModConfig;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.util.Identifier;
 
 public class Reference {
     private static ModConfig config;
     public static final String MOD_ID = "spawnprotectiontweaks";
     @SuppressWarnings("OptionalGetWithoutIsPresent")
     public static final String MOD_VERSION = FabricLoader.getInstance().getModContainer(Reference.MOD_ID).get().getMetadata().getVersion().getFriendlyString();
-    public static final Identifier LANGUAGE_PACKET_IDENTIFIER = new Identifier(MOD_ID, "accepts_language_keys");
 
     public static ModConfig getConfig() {
         return config;
